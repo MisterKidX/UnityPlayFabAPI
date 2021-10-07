aaac5d44328474ac83ee72c0efb660dcece34407
MisterKidX <agesonera@gmail.com>
2021-10-07 14:26:23 +0300

    Added title settings Scriptable Object

---
+++ Assets/CSharpAPI/Management/TitleSettingsSO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayFabAPI.Management
+{
+    [CreateAssetMenu(menuName = "PlayFab/Title Settings SO", fileName = "PlayFab Title Settings")]
+    public class TitleSettingsSO : ScriptableObject
+    {
+        [SerializeField]
+        string _titleID;
+        public string TitleID => _titleID;
+    }
+}
